7cccf8e0caad7bb718bb7bfba00e361764ec03b0
rukkiecodes <rukkiecodes@gmail.com>
2021-09-26 04:13:57 +0100

    can now resize navigation drawer

--- src/components/appComponents/nav/Drawer.vue
+++ src/components/appComponents/nav/Drawer.vue
@@ -3,7 +3,8 @@
     app
     clipped
     id="drawer"
-    width="250"
+    :width="navigation.width"
+    ref="drawer"
     color="#1C1E31"
     v-model="drawer.drawer"
   >
@@ -14,21 +15,19 @@
 <script>
 import Tree from "../Tree.vue";
 import { mapState } from "vuex";
+import { data } from "vue2-ace-editor";
 export default {
+  data: () => ({
+    navigation: {
+      shown: false,
+      width: 200,
+      borderSize: 2,
+    },
+  }),
   components: {
     Tree,
   },
   methods: {
-    removeDrawerBorder() {
-      const navigationBorder = document.querySelectorAll(
-        "#drawer .v-navigation-drawer__border"
-      );
-
-      navigationBorder.forEach((element, i) => {
-        element.style.display = "none";
-      });
-    },
-
     showDrawer() {
       switch (this.$vuetify.breakpoint.name) {
         case "xs":
@@ -43,19 +42,83 @@ export default {
           return (this.drawer.drawer = true);
       }
     },
+
+    setBorderWidth() {
+      let i = this.$refs.drawer.$el.querySelector(
+        ".v-navigation-drawer__border"
+      );
+      i.style.width = this.navigation.borderSize + "px";
+      i.style.cursor = "ew-resize";
+    },
+
+    setEvents() {
+      const minSize = this.navigation.borderSize;
+      const el = this.$refs.drawer.$el;
+      const drawerBorder = el.querySelector(".v-navigation-drawer__border");
+      const vm = this;
+      const direction = el.classList.contains("v-navigation-drawer--right")
+        ? "right"
+        : "left";
+
+      function resize(e) {
+        document.body.style.cursor = "ew-resize";
+        let f =
+          direction === "right"
+            ? document.body.scrollWidth - e.clientX
+            : e.clientX;
+        el.style.width = f + "px";
+      }
+      drawerBorder.addEventListener(
+        "mousedown",
+        (e) => {
+          if (e.offsetX < minSize) {
+            el.style.transition = "initial";
+            document.addEventListener("mousemove", resize, false);
+          }
+        },
+        false
+      );
+
+      document.addEventListener(
+        "mouseup",
+        () => {
+          el.style.transition = "";
+          this.navigation.width = el.style.width;
+          document.body.style.cursor = "";
+          document.removeEventListener("mousemove", resize, false);
+        },
+        false
+      );
+    },
   },
   created() {
-    this.removeDrawerBorder();
     this.showDrawer();
   },
   mounted() {
     this.$nextTick(() => {
-      this.removeDrawerBorder();
       this.showDrawer();
+
+      switch (this.$vuetify.breakpoint.name) {
+        case "xs":
+          return false;
+        case "sm":
+          return false;
+        case "md":
+          return false;
+        case "lg":
+          this.setBorderWidth();
+          this.setEvents();
+        case "xl":
+          this.setBorderWidth();
+          this.setEvents();
+      }
     });
   },
   computed: {
     ...mapState(["drawer"]),
+    direction() {
+      return this.navigation.shown === false ? "Open" : "Closed";
+    },
   },
 };
 </script>
